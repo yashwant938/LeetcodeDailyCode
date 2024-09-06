5269acd9814b418d8a4abb91e3531031da5457fa
YashwantRana23 <yashwant23107@iiitd.ac.in>
2024-09-06 19:50:19 +0530

    LL

---
+++ 3217. Delete Nodes From Linked List Present in Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st(begin(nums), end(nums));
+        while (head != NULL && st.find(head->val) != st.end()) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        
+        ListNode* curr = head;
+        while (curr != NULL && curr->next != NULL) {
+            if (st.find(curr->next->val) != st.end()) {
+                ListNode* temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp;
+            } else {
+                curr = curr->next;
+            }
+        }
+        
+        return head;
+    }
+};
